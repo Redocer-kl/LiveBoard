3676bd1f96763b746dbd119460541c7e8e513429
Redocer-kl <turbocoderwtf@gmail.com>
2025-09-05 15:18:10 +0700

    Redis SetUp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,3 +131,12 @@ CHANNEL_LAYERS = {
         },
     },
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Your React dev server
+    "http://127.0.0.1:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
